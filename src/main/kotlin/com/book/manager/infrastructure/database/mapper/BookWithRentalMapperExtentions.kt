5a20881d3fd77b64d483cc935aafb2af6a890377
niha-2 <gen.3polym.hn@gmail.com>
2025-01-28 18:31:39 +0900

    一覧取得機能: mapper, repository, service, controller作成

---
+++ src/main/kotlin/com/book/manager/infrastructure/database/mapper/BookWithRentalMapperExtentions.kt
@@ -0,0 +1,35 @@
+package com.book.manager.infrastructure.database.mapper
+
+import com.book.manager.infrastructure.database.mapper.BookDynamicSqlSupport.Book
+import com.book.manager.infrastructure.database.mapper.BookDynamicSqlSupport.Book.author
+import com.book.manager.infrastructure.database.mapper.BookDynamicSqlSupport.Book.id
+import com.book.manager.infrastructure.database.mapper.BookDynamicSqlSupport.Book.releaseDate
+import com.book.manager.infrastructure.database.mapper.BookDynamicSqlSupport.Book.title
+import com.book.manager.infrastructure.database.mapper.RentalDynamicSqlSupport.Rental
+import com.book.manager.infrastructure.database.mapper.RentalDynamicSqlSupport.Rental.rentalDatetime
+import com.book.manager.infrastructure.database.mapper.RentalDynamicSqlSupport.Rental.returnDeadline
+import com.book.manager.infrastructure.database.mapper.RentalDynamicSqlSupport.Rental.userId
+import com.book.manager.infrastructure.database.record.BookWithRentalRecord
+import org.mybatis.dynamic.sql.util.kotlin.mybatis3.from
+import org.mybatis.dynamic.sql.SqlBuilder.equalTo
+import org.mybatis.dynamic.sql.SqlBuilder.select
+
+
+private val columnlist = listOf(
+    id,
+    title,
+    author,
+    releaseDate,
+    userId,
+    rentalDatetime,
+    returnDeadline
+)
+
+fun BookWithRentalMapper.select(): List<BookWithRentalRecord> {
+    val selectStatement = select(columnlist).from(Book, "b") {
+        leftJoin(Rental, "r") {
+            on(Book.id, equalTo(Rental.bookId))
+        }
+    }
+    return selectMany(selectStatement)
+}
