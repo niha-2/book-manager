8a18842c428c8c4e50f1d41a932cc95c168d7e4a
niha-2 <gen.3polym.hn@gmail.com>
2025-01-29 11:24:27 +0900

    [add] 登録機能: repository, service, controller追加

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -4,16 +4,20 @@ import com.book.manager.domain.model.Book
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.domain.model.Rental
 import com.book.manager.domain.repository.BookRepository
+import com.book.manager.infrastructure.database.mapper.BookMapper
 import com.book.manager.infrastructure.database.mapper.BookWithRentalMapper
 import com.book.manager.infrastructure.database.mapper.select
+import com.book.manager.infrastructure.database.mapper.insert
 import com.book.manager.infrastructure.database.mapper.selectByPrimaryKey
+import com.book.manager.infrastructure.database.record.BookRecord
 import com.book.manager.infrastructure.database.record.BookWithRentalRecord
 import org.springframework.stereotype.Repository
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @Repository
 class BookRepositoryImpl(
-    private val bookWithRentalMapper: BookWithRentalMapper
+    private val bookWithRentalMapper: BookWithRentalMapper,
+    private val bookMapper: BookMapper
 ): BookRepository {
     override fun findAllWithRental(): List<BookWithRental> {
         return bookWithRentalMapper.select().map { toModel(it) }
@@ -39,5 +43,12 @@ class BookRepositoryImpl(
             )
         }
         return BookWithRental(book, rental)
+    }
+    override fun register(book: Book) {
+        bookMapper.insert(toRecord(book))
+    }
+
+    private fun toRecord(model: Book): BookRecord {
+        return BookRecord(model.id, model.title, model.author, model.releaseDate)
     }
 }
---
+++ src/main/kotlin/com/book/manager/presentation/controller/AdminBookController.kt
@@ -0,0 +1,29 @@
+package com.book.manager.presentation.controller
+
+import RegisterBookRequest
+import com.book.manager.application.service.AdminBookService
+import com.book.manager.domain.model.Book
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("admin/book")
+@CrossOrigin(origins = ["http://localhost:8081"], allowCredentials = "true")
+class AdminBookController(
+    private val adminBookService: AdminBookService
+) {
+    @PostMapping("/register")
+    fun register(@RequestBody request: RegisterBookRequest) {
+        adminBookService.register(
+            Book(
+                request.id,
+                request.title,
+                request.author,
+                request.releaseDate
+            )
+        )
+    }
+}
